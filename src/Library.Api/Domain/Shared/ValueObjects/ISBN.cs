67007c013bd723274b39e3fa6a38019af5785dc6
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-08-26 00:46:42 -0400

    Add ISBN as a value object

---
+++ src/Library.Api/Domain/Shared/ValueObjects/ISBN.cs
@@ -0,0 +1,15 @@
+namespace Library.Api.Domain.Shared.ValueObjects
+{
+    public class ISBN
+    {
+        public string Value { get; init; }
+
+        public ISBN(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException("Invalid value cannot be CPF");
+
+            Value = value;
+        }
+    }
+}
