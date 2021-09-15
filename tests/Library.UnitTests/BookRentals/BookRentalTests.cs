9d95f78aad4ee3b29618db70f2474efa86a10f7c
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-09-14 23:33:00 -0400

    Create test to validate locator penalization

--- tests/Library.UnitTests/BookRentals/BookRentalTests.cs
+++ tests/Library.UnitTests/BookRentals/BookRentalTests.cs
@@ -1,6 +1,8 @@
 using FluentAssertions;
 using Library.Api.Domain.BookRentals;
 using Library.Api.Domain.BookRentals.Users;
+using Library.Api.Domain.Shared;
+using Moq;
 using Xunit;
 using static Library.Api.Domain.BookRentals.BookRental;
 using static Library.Api.Domain.BookRentals.Events.V1;
@@ -9,15 +11,20 @@ namespace Library.UnitTests.BookRentals
 {
     public class BookRentalTests
     {
+        Mock<ISystemClock> _systemClock;
+
         IEnumerable<Book> Books { get; }
 
         public BookRentalTests()
-            => Books = new List<Book>()
+        {
+            Books = new List<Book>()
             {
                 Book.Create("Fight Club", "Chuck Palahniuk", "https://photo"),
                 Book.Create("Fight Club", "Chuck Palahniuk", "https://photo"),
                 Book.Create("Fight Club", "Chuck Palahniuk", "https://photo"),
             };
+            _systemClock = new();
+        }
 
         Librarian Librarian { get; } = Librarian.Create("Vinicius", "Mamoré", new DateTime(1997, 10, 25),
             "0001234567", "Campo Grande", "MS", "Rua Hehehe", "80");
@@ -41,12 +48,27 @@ namespace Library.UnitTests.BookRentals
         [Fact]
         public void Returned_Book()
         {
+            _systemClock.Setup(c => c.UtcNow)
+                .Returns(DateTime.UtcNow);
             var bookRental = BookRental.Create(Librarian, Locator, Books, DateTime.UtcNow.AddDays(14));
             bookRental.Status.Should().Be(BookRentStatus.OnGoing);
-            bookRental.Returned();
+            bookRental.Returned(_systemClock.Object);
             var rentalReturnedEvent = bookRental.GetChanges().Last();
             rentalReturnedEvent.Should().BeOfType<RentalReturned>();
             bookRental.Status.Should().Be(BookRentStatus.Done);
+        }
+
+        [Fact]
+        public void Returned_Book_Late_Should_Penalyze_Locator()
+        {
+            _systemClock.Setup(c => c.UtcNow).Returns(DateTime.UtcNow.AddDays(3));
+            var bookRental = BookRental.Create(Librarian, Locator, Books, DateTime.UtcNow.AddDays(1));
+            bookRental.Status.Should().Be(BookRentStatus.OnGoing);
+            bookRental.Returned(_systemClock.Object);
+            var rentalReturnedEvent = bookRental.GetChanges().Last();
+            rentalReturnedEvent.Should().BeOfType<RentalReturned>();
+            bookRental.Status.Should().Be(BookRentStatus.Done);
+            bookRental.Locator.IsPenalized(_systemClock.Object).Should().BeTrue();
         }
     }
 }
