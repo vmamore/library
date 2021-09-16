c32ab5876833f81c731ed801fea34f647d46b5e7
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-09-16 19:29:29 -0400

    Fix data.sql and add to default docker-compose file

--- sql/data.sql
+++ sql/data.sql
@@ -13,10 +13,10 @@ INSERT INTO rentals.books ("Id", "Title", "Author", "Status", "PhotoUrl") VALUES
 ('59ba2440-1f66-4ac7-8637-f68df6e1758d', 'Choke', 'Chuck Palahniuk', 1, 'https://m.media-amazon.com/images/P/0385720920.01._SCLZZZZZZZ_SX500_.jpg'),
 ('ef0e074f-2789-4635-8668-b6855d4b3973', 'Greenlights', 'Matthew McConaughey', 1, 'https://m.media-amazon.com/images/I/51DZeZw7K0L.jpg');
 
-INSERT INTO inventory.books ("Id", "Title", "Author", "ReleasedYear", "Pages", "Version", "ISBN") VALUES 
-('44bf17e5-ffd2-4219-8211-28a37448a119','Fight Club', 'Chuck Palahniuk', '2005', '218', '1', '978-0393327342'),
-('bb58cd57-2148-4d46-a001-8cc4ce93ad4f','The Almanack of Naval Ravikant', 'Eric Jorgenson', '2020', '244', '1', ''),
-('59ba2440-1f66-4ac7-8637-f68df6e1758d', 'Choke', 'Chuck Palahniuk', '2002', '304', '1', '978-0385720922'),
-('ef0e074f-2789-4635-8668-b6855d4b3973', 'Greenlights', 'Matthew McConaughey', '2020', '304', '1', '978-0593139134');
+INSERT INTO inventory.books ("Id", "Title", "Author", "ReleasedYear", "Pages", "Version", "ISBN", "PhotoUrl") VALUES 
+('44bf17e5-ffd2-4219-8211-28a37448a119','Fight Club', 'Chuck Palahniuk', '2005', '218', '1', '978-0393327342', 'https://m.media-amazon.com/images/P/0393327345.01._SCLZZZZZZZ_SX500_.jpg'),
+('bb58cd57-2148-4d46-a001-8cc4ce93ad4f','The Almanack of Naval Ravikant', 'Eric Jorgenson', '2020', '244', '1', '000-0000000000', 'https://m.media-amazon.com/images/P/B08FF8MTM6.01._SCLZZZZZZZ_SX500_.jpg'),
+('59ba2440-1f66-4ac7-8637-f68df6e1758d', 'Choke', 'Chuck Palahniuk', '2002', '304', '1', '978-0385720922', 'https://m.media-amazon.com/images/P/0385720920.01._SCLZZZZZZZ_SX500_.jpg'),
+('ef0e074f-2789-4635-8668-b6855d4b3973', 'Greenlights', 'Matthew McConaughey', '2020', '304', '1', '978-0593139134', 'https://m.media-amazon.com/images/I/51DZeZw7K0L.jpg');
 
 COMMIT;
