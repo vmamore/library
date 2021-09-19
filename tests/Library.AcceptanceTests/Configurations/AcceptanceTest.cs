cc65d87495aacd17f84c26a18070ae9945212db5
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-09-19 17:34:37 -0400

    Create default folder to set configuration class

---
+++ tests/Library.AcceptanceTests/Configurations/AcceptanceTest.cs
@@ -0,0 +1,40 @@
+﻿using Library.AcceptanceTests.Tests.Configurations;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Library.AcceptanceTests.Configurations
+{
+    public abstract class AcceptanceTest : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private const string INVENTORY_POST_BOOK = "inventory/books";
+
+        private const string RENTALS_GET_ALL_BOOKS = "rentals/books/all";
+
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public AcceptanceTest(CustomWebApplicationFactory<Startup> factory) => _factory = factory;
+
+        protected HttpClient Client
+        {
+            get
+            {
+                return _factory.CreateClient();
+            }
+        }
+
+        protected StringContent CreatePayload(object obj)
+        {
+            var payload = JsonSerializer.Serialize(obj);
+            return new StringContent(payload, Encoding.UTF8, "application/json");
+        }
+
+        public async Task<HttpResponseMessage> CreateBookInInventory(StringContent content) => await Client.PostAsync(INVENTORY_POST_BOOK, content);
+
+        public async Task<HttpResponseMessage> GetAllBooksInRentals(int page = 1, string title = null) => await Client.GetAsync($"{RENTALS_GET_ALL_BOOKS}?page={page}&title={title}");
+
+
+    }
+}
---
+++ tests/Library.AcceptanceTests/Configurations/CustomWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using Library.Api.Infrastructure.BookRentals;
+using Library.Api.Infrastructure.Inventory;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace Library.AcceptanceTests.Tests.Configurations
+{
+    public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var inventoryDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<InventoryDbContext>));
+
+                services.Remove(inventoryDescriptor);
+
+                services.AddDbContext<InventoryDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                var bookRentalDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<BookRentalDbContext>));
+
+                services.Remove(bookRentalDescriptor);
+
+                services.AddDbContext<BookRentalDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+            });
+        }
+    }
+}
