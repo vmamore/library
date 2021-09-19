ba2bc70595d58be39768ab8ce0342f5710bfef92
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-09-19 17:35:16 -0400

    Create test for Rental endpoint

---
+++ tests/Library.AcceptanceTests/Tests/RentalsApiTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Library.AcceptanceTests.Configurations;
+using Library.AcceptanceTests.Tests.Configurations;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Library.AcceptanceTests.Tests
+{
+    public class RentalsApiTests : AcceptanceTest
+    {
+        public RentalsApiTests(CustomWebApplicationFactory<Startup> factory) : base(factory) { }
+
+        [Fact]
+        public async Task PostRental_ShouldReturn_200()
+        {
+        }
+    }
+}
