e2d17bab70a16023345e8145892b7e9a3fd157c6
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-09-21 23:17:16 -0400

    Create unit test informing invalid values

--- tests/Library.UnitTests/BookRentals/LibrarianTests.cs
+++ tests/Library.UnitTests/BookRentals/LibrarianTests.cs
@@ -18,5 +18,16 @@ namespace Library.UnitTests.BookRentals
             var librarianCreatedEvent = librarian.GetChanges().Last();
             librarianCreatedEvent.Should().BeOfType<LibrarianCreated>();
         }
+
+        [Theory]
+        [InlineData(null, "Vinicius", "Mamoré")]
+        [InlineData("0001234567", null, "Mamoré")]
+        [InlineData("0001234567", "Vinicius", null)]
+        public void Should_Throw_Exception_When_Passing_Invalid_Value(string cpf, string firstName, string lastName)
+        {
+            var action = () => Librarian.Create(firstName, lastName, new DateTime(1997, 10, 25),
+            cpf, "Campo Grande", "MS", "Rua Hehehe", "80");
+            action.Should().Throw<Exception>();
+        }
     }
 }
